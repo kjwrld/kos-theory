aa271df87c2a7f026365c5e73851e4df9bd6073e
KJ <kj@KJs-MacBook-Pro.lan>
2025-01-16 01:12:27 -0800

    applying styles directly to page

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,11 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: "export", // Enables static export
-  basePath: isProd ? "/kos-theory" : "", // Adjust for GitHub Pages
-  assetPrefix: isProd ? "/kos-theory/" : "", // Adjust for GitHub Pages
+  output: "export",
+  basePath: isProd ? "/kos-theory" : "",
+  assetPrefix: isProd ? "/kos-theory/" : "",
   images: {
-    unoptimized: true, // Disable Image Optimization for static export
+    unoptimized: true,
   },
 };
 
