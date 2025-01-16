ecaf9f2255c8b205aa4bac5d2c5e37ab79b44f04
KJ <kj@KJs-MacBook-Pro.lan>
2025-01-15 23:50:14 -0800

    wow its taking a long time to set up a landing page lol

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/kos-theory", 
-  assetPrefix: "/kos-theory", 
-};
-
-export default nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export", // Enables static export
+  basePath: isProd ? "/your-repo-name" : "", // Adjust for GitHub Pages
+  assetPrefix: isProd ? "/your-repo-name/" : "", // Adjust for GitHub Pages
+  images: {
+    unoptimized: true, // Disable Image Optimization for static export
+  },
+};
 
 export default nextConfig;
